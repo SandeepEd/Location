af5236c1743b00f05f3f2f8ef0c7cb7f17bcd0a9
Sandeep Edara <sunny.edara09@gmail.com>
2022-11-28 00:03:45 -0500

    format code

--- Location.js
+++ Location.js
@@ -1,24 +1,31 @@
 import React, { useState, useRef } from "react";
-import { ActivityIndicator, Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  ActivityIndicator,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
 import * as LocationServices from "expo-location";
 
 const Location = () => {
   const mapRef = useRef(null);
-  const [ isLoading, setIsLoading ] = useState(false); 
+  const [isLoading, setIsLoading] = useState(false);
   const [region, setRegion] = useState({
     latitude: 16.089809,
     longitude: 80.169601,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   });
+  
   const handleOnClick = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     let { status } = await LocationServices.requestForegroundPermissionsAsync();
     if (status !== "granted") {
       setErrorMsg("Permission to access location was denied");
-      setIsLoading(false)
+      setIsLoading(false);
       return;
     }
     const { coords } = await LocationServices.getCurrentPositionAsync();
@@ -33,6 +40,7 @@ const Location = () => {
     setRegion(currentRegion);
     mapRef.current.animateToRegion(currentRegion, 3 * 1000);
   };
+
   return (
     <View style={styles.container}>
       <MapView
@@ -43,10 +51,22 @@ const Location = () => {
       >
         <Marker pinColor="green" coordinate={region} />
       </MapView>
-      <TouchableOpacity onPress={handleOnClick} style={{ height: 40, width: '80%', justifyContent: 'center', alignItems: 'center',
-         marginTop: 10, backgroundColor: '#24a0ed', borderRadius: '5px' }}>
-        <Text style={styles.locationText}>Go To My Location
-        {isLoading && <ActivityIndicator color={"white"} />}</Text>
+      <TouchableOpacity
+        onPress={handleOnClick}
+        style={{
+          height: 40,
+          width: "80%",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 10,
+          backgroundColor: "#24a0ed",
+          borderRadius: "5px",
+        }}
+      >
+        <Text style={styles.locationText}>
+          Go To My Location
+          {isLoading && <ActivityIndicator color={"white"} />}
+        </Text>
       </TouchableOpacity>
       <Text style={styles.text}>Current latitude: {region.latitude}</Text>
       <Text style={styles.text}>Current longitude: {region.longitude}</Text>
@@ -62,11 +82,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
-    paddingBottom: '7%'
+    paddingBottom: "7%",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
-    height: '90%'
+    height: "90%",
   },
   buttonStyle: {
     position: "relative",
@@ -74,10 +94,10 @@ const styles = StyleSheet.create({
     marginBottom: 100,
   },
   locationText: {
-    color: 'white',
+    color: "white",
     fontSize: "20",
   },
-  text:{
-    fontSize: '20'
-  }
+  text: {
+    fontSize: "20",
+  },
 });
